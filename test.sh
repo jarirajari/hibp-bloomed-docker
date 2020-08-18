c1ff211c91f923dead11ea4622278d0dc7caa3e3
jari <mr.jari.kuusisto@gmail.com>
2020-08-18 18:23:59 +0300

    Changed test to use hash in the test data set

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl -XGET -i http://localhost:10101/check?admin
-curl -XGET -i http://localhost:10101/check-sha1?D033E22AE348AEB5660FC2140AEC35850C4DA997
+curl -XGET -i http://localhost:10101/check-sha1?9D3DF7C75CC5B8ECE51E28772BF953D96CAB3A9E
 
